9bd0a1f1e8027923c90285b894fa5be32ea4dd18
joaovitmac <joaovmachado10@gmail.com>
2021-07-14 10:39:05 -0300

    implemented ParseSync

--- src/index.ts
+++ src/index.ts
@@ -16,32 +16,35 @@ export interface CompileOptions {
 export function Parse(code: string, options: CompileOptions): Promise<string> {
     return new Promise((resolve, reject) => {
         try {
-            if (!code?.length) return null;
-
-            if (options.max_length && code.length > options.max_length)
-                return reject(new Error("too larger"));
-
-            if (options.cache !== false) {
-                const cached_code = CacheGet(code);
-                if (cached_code) return resolve(cached_code);
-            }
-
-            const parser = new Parser(grammar);
-            parser.feed(code);
-
-            let result = parser.results?.[0];
-
-            if (!result) reject(new Error("nothing to parse"));
-            else {
-                if (options.transformer) {
-                    result = options.transformer.transform(result);
-                    resolve(result);
-                } else resolve(result);
-            }
-
-            if (options.cache !== false) CachePut(code, result);
+            const result = ParseSync(code, options);
+            resolve(result);
         } catch (error) {
             reject(error);
         }
     });
+}
+
+export function ParseSync(code: string, options: CompileOptions): string {
+    if (!code?.length) return "";
+
+    if (options.max_length && code.length > options.max_length)
+        throw new Error("too larger");
+
+    if (options.cache !== false) {
+        const cached_code = CacheGet(code);
+        if (cached_code) return cached_code;
+    }
+
+    const parser = new Parser(grammar);
+    parser.feed(code);
+
+    let result = parser.results?.[0];
+
+    if (!result) throw new Error("nothing to parse");
+
+    if (options.transformer) result = options.transformer.transform(result);
+
+    if (options.cache !== false) CachePut(code, result);
+
+    return result;
 }
